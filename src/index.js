bf7a2544ab5f209e5a468e65dcf5f232048f7595
zhuxianda <619127324@qq.com>
2017-09-10 13:12:51 +0800

    init commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import _ from 'lodash';
+import './style.css';
+import Icon from './icon.jpeg';
+import Data from './data.xml';
+
+function component() {
+  var element = document.createElement('div');
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+   // 将图像添加到我们现有的 div。
+   var myIcon = new Image();
+   myIcon.src = Icon;
+
+   element.appendChild(myIcon);
+console.log(Data);
+
+    return element;
+}
+
+  document.body.appendChild(component());
